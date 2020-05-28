fe9f6f6e802bd6e5c6e011a9cdf25760824c38a9
John B <john@nexys.ch>
2020-05-28 23:46:22 +0200

    publish3

---
+++ docs/precache-manifest.2b867180f414a24b232fe20303c18ee3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328f6fa5d91c39aa1e0cbc693f3b3032",
+    "url": "/react-map/index.html"
+  },
+  {
+    "revision": "37729168e38bb964ff52",
+    "url": "/react-map/static/js/2.4192942b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-map/static/js/2.4192942b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b082f0b27844687162",
+    "url": "/react-map/static/js/main.7bd6e8e9.chunk.js"
+  },
+  {
+    "revision": "7e68bdfb0e8be19f2a4d",
+    "url": "/react-map/static/js/runtime-main.9af5c50a.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a9db8e9855e9beb6aafa04fd06fa73c.js"
+  "/react-map/precache-manifest.2b867180f414a24b232fe20303c18ee3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-map/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
